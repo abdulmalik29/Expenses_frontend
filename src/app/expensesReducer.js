80096dba20cdd676440f3f4e9c815ed6d8da948a
abdulmalik alghazi <malka2974@gmail.com>
2021-08-04 21:19:42 +0400

    few set up changes

---
+++ src/app/expensesReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    expenses: [],
+}
+
+export const ActionType = {
+    SET_EXPENSES: 'SET_EXPENSES',
+}
+
+export const ActionCreators = {
+    setExpenses: payload => ({ type: ActionType.SET_EXPENSES, payload })
+}
+
+export default (state = initialState, action) => {
+    switch(action.type){
+        case ActionType.SET_EXPENSES:
+            return { ...state, expenses: [...action.payload]}
+    }
+}
---
+++ src/services/expenses.js
@@ -0,0 +1,15 @@
+export const GetExpenses = async (dispatch) =>{
+    try{
+        // api call
+
+        const expenses = [
+            { id = 1, type = "food" , amount = 3.14}, 
+            { id = 2, type = "gas", amount = 2 },
+            { id = 3, type = "transport", amount = 5.3 }
+        
+        ];
+        
+    } catch{
+        console.log("Error")
+    }
+};
